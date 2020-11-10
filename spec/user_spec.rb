5897c08496e7c4f3aeff55b23f58d8fbfdbf5a16
yusuke39 <hiranoyusuke39@gmail.com>
2020-11-10 18:39:19 +0900

    テストコードリファクタリング

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,21 +2,15 @@ require_relative '../lib/user'
 
 RSpec.describe User do
   describe '#greet' do
-    let(:user) {User.new(params)}
-    let(:params) {{name: 'ゆーすけ', age: age}}
+    let(:user) {User.new(name: 'ゆーすけ', age: age)}
+    subject {user.greet}
     context '12歳以下の場合' do
       let(:age) { 12 }
-      it 'ひらがなで答えること' do
-        user = User.new(params)
-        expect(user.greet).to eq 'ぼくはゆーすけです'
-      end
+      it { is_expected.to eq 'ぼくはゆーすけです' }
     end
     context '13歳以上の場合' do
       let(:age) { 13 }
-      it '漢字で答えること' do
-        user = User.new(params)
-        expect(user.greet).to eq '僕はゆーすけです'
-      end
+      it { is_expected.to eq '僕はゆーすけです'}
     end
   end
 end
